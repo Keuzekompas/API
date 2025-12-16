0c22f51d1f3ce17f62b09221bf5c827ff77f84c8
Sietsethooft <147145688+Sietsethooft@users.noreply.github.com>
2025-12-16 14:26:00 +0100

    Errorhandler update
    Created a new errorhandler and added it to user controller

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,10 +1,10 @@
 /* eslint-disable @typescript-eslint/no-unsafe-argument */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
-
 import { Controller, Get, HttpCode, Request } from '@nestjs/common';
 import { UserService } from '../services/user.service';
 import { User } from '../schemas/user.schema';
+import { handleError } from '../utils/error-handler';
 
 @Controller('api/user')
 export class UserController {
@@ -13,8 +13,13 @@ export class UserController {
   @Get('profile')
   @HttpCode(200)
   async getProfile(@Request() req): Promise<User | null> {
-    const userId = req.user?.userId;
-    if (!userId) return null;
-    return await this.userService.findById(userId);
+    try {
+      const userId = req.user?.userId;
+      if (!userId) return null;
+      return await this.userService.findById(userId);
+    } catch (error) {
+      handleError(error, 'UserController.getProfile');
+      throw error;
+    }
   }
 }
