08d35d8694a915d522f38b5945b800d4bd105fa5
Sietsethooft <147145688+Sietsethooft@users.noreply.github.com>
2025-12-17 10:45:26 +0100

    Database connection

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
-import { UserModule } from './user.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+
+import { UserModule } from './user.module';
 import { AppController } from '../controllers/app.controller';
 import { AppService } from '../services/app.service';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env'],
+    }),
     MongooseModule.forRoot(
       process.env.MONGODB_URI ?? 'mongodb://localhost:27017/avanskeuzekompas',
     ),
