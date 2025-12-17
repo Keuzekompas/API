6d9bcc2d021c3bf6e4f84d8255890ff86185d079
Sietsethooft <147145688+Sietsethooft@users.noreply.github.com>
2025-12-17 11:03:59 +0100

    Revert DB connection

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,22 +1,10 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule } from '@nestjs/config';
-
 import { UserModule } from './user.module';
 import { AppController } from '../controllers/app.controller';
 import { AppService } from '../services/app.service';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      envFilePath: ['.env'],
-    }),
-    MongooseModule.forRoot(
-      process.env.MONGODB_URI ?? 'mongodb://localhost:27017/avanskeuzekompas',
-    ),
-    UserModule,
-  ],
+  imports: [UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
