2187427fa838cd74f647c13222425bf314de9228
TheLobbyPigeon <gijsvanvugt2002@gmail.com>
2025-12-17 11:10:44 +0100

    Renamed Login to Auth, added Bcrypt to password checks and nearly full login functionality

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from '../controllers/app.controller';
 import { AppService } from '../services/app.service';
+import { ConfigModule } from '@nestjs/config';
+import { Mongoose } from 'mongoose';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    // MongooseModule.forRoot(process.env.MONGO_URI),
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,14 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { UserRepository } from '../repositories/user.repository';
 import { UserDocument } from '../schemas/user.schema';
-import { LoginDto } from '../dtos/login.dto';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class LoginService {
+export class AuthService {
   constructor(private readonly userRepository: UserRepository) {}
   async login(email: string, password: string): Promise<UserDocument> {
     const user = await this.userRepository.findByEmail(email);
-    if (!user || user.password !== password) {
+    const passwordMatch = user
+      ? await bcrypt.compare(password, user.password)
+      : false;
+    if (!user || !passwordMatch) {
       throw new Error('Invalid email or password');
     }
     return user;
