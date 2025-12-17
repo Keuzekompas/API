b116ec3a96e6656bbce189fb07574838b2a5d6ae
Sietsethooft <147145688+Sietsethooft@users.noreply.github.com>
2025-12-17 14:13:43 +0100

    Eslint be gone

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,3 @@
-/* eslint-disable @typescript-eslint/no-floating-promises */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as MongoSanitize from 'express-mongo-sanitize';
