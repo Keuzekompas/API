4cc6683a06ec52cd83ad592d589b1b778cdd4c76
Sietsethooft <147145688+Sietsethooft@users.noreply.github.com>
2026-01-06 12:20:17 +0100

    Refactor express-mongo-sanitize middleware usage
    Changed import of express-mongo-sanitize to use wildcard import and moved the custom sanitization middleware earlier in the middleware chain. Removed duplicate middleware registration at the end of the bootstrap function.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import MongoSanitize from 'express-mongo-sanitize';
+import * as MongoSanitize from 'express-mongo-sanitize';
 import cookieParser from 'cookie-parser';
 import { ApiExceptionFilter } from './utils/api-exception.filter';
 import { ValidationPipe } from '@nestjs/common';
@@ -9,7 +9,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use(cookieParser());
-  app.use(MongoSanitize());
+
+  app.use((req, res, next) => {
+    if (req.body) MongoSanitize.sanitize(req.body);
+    if (req.params) MongoSanitize.sanitize(req.params);
+    if (req.query) MongoSanitize.sanitize(req.query);
+    next();
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -39,13 +45,6 @@ async function bootstrap() {
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
     maxAge: 600,
-  });
-
-  app.use((req, res, next) => {
-    if (req.body) MongoSanitize.sanitize(req.body);
-    if (req.params) MongoSanitize.sanitize(req.params);
-    if (req.query) MongoSanitize.sanitize(req.query);
-    next();
   });
 
   await app.listen(process.env.PORT ?? 3000);
