645becf78207e86e9e147a9a9d86fac7ad4d2841
Clemens <100091164+Clemens00505@users.noreply.github.com>
2026-01-08 10:52:11 +0100

    [FIX] - Added redis password

--- src/utils/redis.ts
+++ src/utils/redis.ts
@@ -5,4 +5,5 @@ dotenv.config();
 export const redisInstance = new Redis({
   host: process.env.REDIS_HOST || 'localhost',
   port: Number(process.env.REDIS_PORT) || 6379,
+  password: process.env.REDIS_PASSWORD || undefined,
 });
