10d4b23ec9cde9d84dcb1bbe54e4cc0bef7147c2
Clemens <100091164+Clemens00505@users.noreply.github.com>
2026-01-08 21:23:38 +0100

    Delete studentNumber because unnecessary

--- src/user/user.interface.ts
+++ src/user/user.interface.ts
@@ -2,7 +2,6 @@ import { Module } from '../modules/module.interface';
 
 export interface UserInterface {
   id: string;
-  studentNumber: number;
   email: string;
   name: string;
   favoriteModules: string[] | Module[];
